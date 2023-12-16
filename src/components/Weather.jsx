7d1337e837a35ffeb517f0b24706b9b571359071
akansha-1202 <akanshav325@gmail.com>
2023-12-17 01:35:09 +0530

    changes for setting the height

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -71,9 +71,9 @@ function Weather() {
       };
 
       return (
-            <div className="w-full sm:h-[100vh] bg-gradient-to-b from-[#2493DF] from-10% to-[#A2ABBA] grid place-items-center lg:py-2">
+            <div className="w-full sm:h-[100vh] bg-gradient-to-b from-[#2493DF] from-10% to-[#A2ABBA] grid place-items-center py-2">
               {/* center box */}
-                <div className="w-[84%] sm:w-[62%] h-[92vh] lg:h-[83vh] border-2 border-white rounded-lg shadow-md shadow-white px-4 py-4 flex flex-col justify-between">
+                <div className="w-[84%] sm:w-[62%] h-[100vh] lg:h-[83vh] border-2 border-white rounded-lg shadow-md shadow-white px-4 py-4 flex flex-col justify-between">
                     {/* search box */}
                     <Form city={city} setCity={setCity} handleCitySearch={handleCitySearch} loadings={loadings}/>
 
